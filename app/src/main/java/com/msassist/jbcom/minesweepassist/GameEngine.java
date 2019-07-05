b589ca80f4ff129fa4208706de8e5b47eebdbe2c
jzrlza <parinvut12345@gmail.com>
2019-07-05 10:29:11 +0700

    Log test for print grid is complete, also extends activity in main.

--- app/src/main/java/com/msassist/jbcom/minesweepassist/GameEngine.java
+++ app/src/main/java/com/msassist/jbcom/minesweepassist/GameEngine.java
@@ -1,6 +1,9 @@
 package com.msassist.jbcom.minesweepassist;
 
 import android.content.Context;
+import android.util.Log;
+import com.msassist.jbcom.minesweepassist.util.Generator;
+import com.msassist.jbcom.minesweepassist.util.PrintGrid;
 
 /**
  *
@@ -11,6 +14,9 @@ public class GameEngine {
     private static GameEngine instance;
 
     private Context context;
+    private static final int BOMBS = 20;
+    private static final int WIDTH = 8;
+    private static final int HEIGHT = 8;
 
     public static GameEngine getInstance() {
         if (instance == null) {
@@ -22,8 +28,11 @@ public class GameEngine {
     private GameEngine() {}
 
     public void createGrid(Context context) {
+        Log.e("GameEngine","createGrid is working");
         this.context = context;
 
-
+        // create the grid and store it
+        int[][] GeneratedGrid = Generator.generate(BOMBS, WIDTH, HEIGHT);
+        PrintGrid.print(GeneratedGrid,WIDTH,HEIGHT);
     }
 }
---
+++ app/src/main/java/com/msassist/jbcom/minesweepassist/util/PrintGrid.java
@@ -0,0 +1,15 @@
+package com.msassist.jbcom.minesweepassist.util;
+
+public class PrintGrid {
+
+    public static void print( final int[][] grid , final int width , final int height ){
+        for( int x = 0 ; x < width ; x++ ){
+            String printedText = "| ";
+            for( int y = 0 ; y < height ; y++ ){
+                printedText += String.valueOf(grid[x][y]).replace("-1", "B") + " | ";
+            }
+            System.out.print(printedText);
+
+        }
+    }
+}
