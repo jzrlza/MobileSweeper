bc9e82220baf78ec571fff0383b468f7b79a6763
jzrlza <parinvut12345@gmail.com>
2019-07-05 09:57:28 +0700

    Init project.

---
+++ app/src/main/java/com/msassist/jbcom/minesweepassist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.msassist.jbcom.minesweepassist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
